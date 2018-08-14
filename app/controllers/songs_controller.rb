bf3151d8b0e6040ebf8089ce7661b999cafcde3a
github username <github email address>
2018-08-14 14:29:16 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,13 +9,16 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
     @song = Song.new(song_params)
-
+    # binding.pry
+    # byebug
     if @song.save
-      redirect_to @song
+      # binding.pry
+      redirect_to song_path(@song)
     else
       render :new
     end
@@ -47,7 +50,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,35 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre_id = Genre.find_or_create_by(name: name).id
+  end
+
+  def genre_name
+    self.genre_id ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist_id = Artist.find_or_create_by(name: name).id
+  end
+
+  def artist_name
+    self.artist_id ? self.artist.name : nil
+  end
+
+  def note_contents=(content)
+    # binding.pry
+    # byebug
+    content = content.select { |one_note_content| one_note_content != ""}
+    content.each { |note_info| self.notes << Note.find_or_create_by(content: note_info) }
+    self.save
+    # binding.pry
+  end
+
+  def note_contents
+    # binding.pry
+    self.notes ? self.notes.map{ |note| note.content } : nil
+  end
+
+end
